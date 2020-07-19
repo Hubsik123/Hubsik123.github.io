3562fd895f43f80fcc92610a857e9fa1e45a0a17
Hubsik <hubsik321@gmail.com>
2020-07-19 22:49:23 +0200

    Ogromna aktualizacja

--- Stronka/Przegladarka_Obrazkow.js
+++ Stronka/Przegladarka_Obrazkow.js
@@ -1,6 +1,41 @@
 var CzyPowiekszonoObrazek = false;
 function Powiekszenie_Obrazka(imgs)
 {
+    if (CzyPowiekszonoObrazek == false) // Otwarcie obrazka
+    {
+            // Wybranie obrazka do powiększenia
+        var Powiekszony_Obrazek = document.getElementById("Powiekszony_Obrazek");
+            // Wybranie tekstu na obrazku
+        var Opis_Obrazka = document.getElementById("Opis_Obrazka");
+            // "Przekopiowanie" scr obrazka do tego powiększonego
+        Powiekszony_Obrazek.src = imgs.src;
+            // Zamiana "alt" na tekst wyświetlany na obrazku
+        Opis_Obrazka.innerHTML = imgs.alt;
+            // Wyświetlenia obrazka
+        Powiekszony_Obrazek.parentElement.style.display = "block";
+            // Licznik
+        CzyPowiekszonoObrazek = true;
+    }
+    else // Zamknięcie obrazka
+    {
+            // Wybranie obrazka do powiększenia
+        var Powiekszony_Obrazek = document.getElementById("Powiekszony_Obrazek");
+            // Wybranie tekstu na obrazku
+        var Opis_Obrazka = document.getElementById("Opis_Obrazka");
+            // "Przekopiowanie" scr obrazka do tego powiększonego
+        Powiekszony_Obrazek.src = imgs.src;
+            // Zamiana "alt" na tekst wyświetlany na obrazku
+        Opis_Obrazka.innerHTML = imgs.alt;
+            // Zamknięcie obrazka
+        Powiekszony_Obrazek.parentElement.style.display = "none";
+            // Licznik
+        CzyPowiekszonoObrazek = false;
+    }
+}
+/*
+var CzyPowiekszonoObrazek = false;
+function Powiekszenie_Obrazka(imgs)
+{
     if (CzyPowiekszonoObrazek == 0) // Otwarcie obrazka
     {
             // Wybranie obrazka do powiększenia
@@ -32,4 +67,4 @@ function Powiekszenie_Obrazka(imgs)
         CzyPowiekszonoObrazek = false;
     }
 }
-    
+*/
