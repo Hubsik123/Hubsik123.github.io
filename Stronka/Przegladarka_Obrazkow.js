b0d523cc60652f99f88c3ef6c220384e8c84733d
Hubsik <hubsik321@gmail.com>
2020-07-22 22:58:18 +0200

    Aktualizacja

--- Stronka/Przegladarka_Obrazkow.js
+++ Stronka/Przegladarka_Obrazkow.js
@@ -1,6 +1,6 @@
 function Wersja_Stronki()
 {
-    var Wersja = "v 13";
+    var Wersja = "v 14";
     alert("To jest wersja strony nr:\n"+Wersja+"\nMam tylko nadzieję że będę pamiętać żeby atkualizować ten licznik.")
 }
 function Feedback()
