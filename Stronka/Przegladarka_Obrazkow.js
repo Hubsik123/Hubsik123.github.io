0d746ec67e15555bec80e71ab51baf8bbba98ca3
Hubsik <hubsik321@gmail.com>
2020-07-23 19:16:21 +0200

    Aktualizacja

--- Stronka/Przegladarka_Obrazkow.js
+++ Stronka/Przegladarka_Obrazkow.js
@@ -1,11 +1,11 @@
 function Wersja_Stronki()
 {
-    var Wersja = "v 14";
+    var Wersja = "v 15";
     alert("To jest wersja strony nr:\n"+Wersja+"\nMam tylko nadzieję że będę pamiętać żeby atkualizować ten licznik.")
 }
 function Feedback()
 {
-    alert("Jeśli podoba ci się ta strona, przydały ci sie te notatki, kod tej strony to daj znać znajomym. Tylko proszę nie kopiować na chama tych wszystkich materiałów.\nJesli zobaczysz jakieś błędy to proszę daj o tym znać. Mój adres e-mail:\nhubsik321@gmail.com")
+    alert("Jeśli podoba ci się ta strona, przydały ci sie te notatki, kod tej strony to daj znać znajomym, im też może się przydać. Tylko proszę nie kopiować na chama tych wszystkich materiałów, dużo pracy na to wszystko poszło.\nJesli zobaczysz jakieś błędy to proszę daj o tym znać. Mój adres e-mail:\nhubsik321@gmail.com")
 }
 function Powiekszenie_Obrazka(imgs)
 {
