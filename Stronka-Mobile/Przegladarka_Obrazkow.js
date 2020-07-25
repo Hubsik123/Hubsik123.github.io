015ff8459dcab3d29947ece15a8e5837487360af
Hubsik <hubsik321@gmail.com>
2020-07-25 18:57:16 +0200

    Aktualizacja

--- Stronka-Mobile/Przegladarka_Obrazkow.js
+++ Stronka-Mobile/Przegladarka_Obrazkow.js
@@ -1,6 +1,6 @@
 function Wersja_Stronki()
 {
-    var Wersja = "v 22 - wersja Mobile";
+    var Wersja = "v 23 - wersja Mobile";
     alert("To jest wersja strony nr:\n"+Wersja+"\nMam tylko nadzieję że będę pamiętać żeby atkualizować ten licznik.")
 }
 function Feedback()
