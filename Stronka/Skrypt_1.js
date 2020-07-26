ef6cb5d567dca4af23109c59b6ba9532da067b21
Hubsik <hubsik321@gmail.com>
2020-07-26 17:50:47 +0200

    Zajebiście duża aktualizacja

--- Stronka/Skrypt_1.js
+++ Stronka/Skrypt_1.js
@@ -1,6 +1,6 @@
 function Wersja_Stronki()
 {
-    var Wersja = "v 25 - wersja Mobile";
+    var Wersja = "v 26 - wersja Mobile";
     alert("To jest wersja strony nr:\n"+Wersja+"\nMam tylko nadzieję że będę pamiętać żeby atkualizować ten licznik.")
 }
 function Feedback()
@@ -28,3 +28,106 @@ function Zamkniecie_Obrazka(imgs) // PRAWIE to samo to funkcja Powiekszenie_Obra
     Opis_Obrazka.innerHTML = imgs.alt;
     Powiekszony_Obrazek.parentElement.style.display = "none";
 }
+var WIP = 0;
+function MenuMobile()
+{
+    var Banner = document.getElementById("Banner");
+    var Linki = document.getElementById("LinkiMobile");
+    var Content = document.getElementById("Content");
+    var Footer = document.getElementById("Footer");
+    if (Linki.style.display === "block")
+    {
+        Linki.style.display = "none";
+        Banner.style.height = "10%";
+        Content.style.height = "85%";
+        Footer.style.height = "5%";
+        WIP = 0;
+    }
+    else
+    {
+        Linki.style.display = "block";
+        Banner.style.height = "10%";
+        Linki.style.height = "85%";
+        Content.style.display = "none";
+        Footer.style.height = "5%";
+        WIP = 1;
+    }
+    /*if (WIP == 0)
+    {
+        Linki.style.display = "block";
+        Banner.style.height = "10%";
+        Linki.style.height = "85%";
+        Content.style.display = "none";
+        Footer.style.height = "5%";
+        WIP = 1;
+    }
+    else
+    {
+        Linki.style.display = "none";
+        Banner.style.height = "10%";
+        Content.style.height = "85%";
+        Footer.style.height = "5%";
+        WIP = 0;
+    }*/
+    if (WIP == 0) // Dzięki temu nie wywala błedu jak kliknie się ikonki 2 razy z rzędu
+    {
+        Linki.style.display = "none";
+        Banner.style.height = "10%";
+        Content.style.display = "block";
+        Content.style.height = "85%";
+        Footer.style.height = "5%";
+    }
+}
+function MobileContent()
+{
+    var Banner = document.getElementById("Banner");
+    var Linki = document.getElementById("LinkiMobile");
+    var Content = document.getElementById("Content");
+    var Footer = document.getElementById("Footer");
+    Linki.style.display = "none";
+    Banner.style.height = "10%";
+    Content.style.display = "block";
+    Content.style.height = "85%";
+    Footer.style.height = "5%";
+}
+/*
+function MenuMobile()
+{
+    var Banner = document.getElementById("Banner");
+    var Linki = document.getElementById("LinkiMobile");
+    var Content = document.getElementById("Content");
+    var Footer = document.getElementById("Footer");
+    if (Linki.style.display === "block")
+    {
+        Linki.style.display = "none";
+        Linki.style.display = "block";
+        Banner.style.height = "10%";
+        Linki.style.height = "0%";
+        Content.style.height = "85%";
+        Footer.style.height = "5%";
+    }
+    else
+    {
+        Linki.style.display = "none";
+        Banner.style.height = "10%";
+        Linki.style.height = "30%";
+        Content.style.height = "55%";
+        Footer.style.height = "5%";
+    }
+}
+*/
+
+/*
+function MenuMobile()
+{
+    var Linki = document.getElementById("LinkiMobile");
+    if (Linki.style.display === "block")
+    {
+        Linki.style.display = "none";
+    }
+    else
+    {
+        Linki.style.display = "block";
+    }
+}
+*/
