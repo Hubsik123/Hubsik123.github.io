420d9447aea74f5b67e832f5db891794e587562a
Hubsik <hubsik321@gmail.com>
2020-07-27 14:17:58 +0200

    Aktualizacja

--- Stronka/Skrypt_1.js
+++ Stronka/Skrypt_1.js
@@ -1,6 +1,6 @@
 function Wersja_Stronki()
 {
-    var Wersja = "v 31";
+    var Wersja = "v 32";
     alert("To jest wersja strony nr:\n"+Wersja+"\nMam tylko nadzieję że będę pamiętać żeby atkualizować ten licznik.")
 }
 function Feedback()
