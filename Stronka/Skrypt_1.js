5e29161b689dfea41474164abd0c2954aded9502
Hubsik <hubsik321@gmail.com>
2020-07-27 16:48:39 +0200

    Aktualizacja

--- Stronka/Skrypt_1.js
+++ Stronka/Skrypt_1.js
@@ -1,6 +1,6 @@
 function Wersja_Stronki()
 {
-    var Wersja = "v 32";
+    var Wersja = "v 33";
     alert("To jest wersja strony nr:\n"+Wersja+"\nMam tylko nadzieję że będę pamiętać żeby atkualizować ten licznik.")
 }
 function Feedback()
@@ -28,7 +28,7 @@ function Zamkniecie_Obrazka(imgs) // PRAWIE to samo to funkcja Powiekszenie_Obra
     Opis_Obrazka.innerHTML = imgs.alt;
     Powiekszony_Obrazek.parentElement.style.display = "none";
 }
-var WIP = 0; //Niby to całe menu na tel to niby z internetu skopiowane https://www.w3schools.com/howto/howto_js_dropdown.asp, ale to to trzeba praktycznie całe przerobić żeby do czego się nadawało...
+var WIP = false; //Niby to całe menu na tel to niby z internetu skopiowane https://www.w3schools.com/howto/howto_js_dropdown.asp i https://www.w3schools.com/howto/howto_js_mobile_navbar.asp, ale to to trzeba praktycznie całe przerobić żeby do czego się nadawało... Trzeba było napisać 3 funkcje żeby to działało...
 function MenuMobile()
 {
     var Banner = document.getElementById("Banner");
@@ -41,7 +41,7 @@ function MenuMobile()
         Banner.style.height = "10%";
         Content.style.height = "85%";
         Footer.style.height = "5%";
-        WIP = 0;
+        WIP = false;
     }
     else
     {
@@ -50,9 +50,9 @@ function MenuMobile()
         Linki.style.height = "85%";
         Content.style.display = "none";
         Footer.style.height = "5%";
-        WIP = 1;
+        WIP = true;
     }
-    if (WIP == 0) // Dzięki temu nie wywala błedu jak kliknie się ikonki 2 razy z rzędu
+    if (WIP == false) // Dzięki temu nie wywala błedu jak kliknie się ikonki 2 razy z rzędu
     {
         Linki.style.display = "none";
         Banner.style.height = "10%";
@@ -63,6 +63,25 @@ function MenuMobile()
 }
 function MobileContent()
 {
+    var Banner = document.getElementById("Banner");
+    var Linki = document.getElementById("LinkiMobile");
+    var Content = document.getElementById("Content");
+    var Footer = document.getElementById("Footer");
+    Linki.style.display = "none";
+    Banner.style.height = "10%";
+    Content.style.display = "block";
+    Content.style.height = "85%";
+    Footer.style.height = "5%";
+}
+var Zoom = false;
+function ZoomFix()
+{
+    if (Zoom == false)
+    {
+        alert("Naprawdę... zmieniełeś zoom żeby zobaczć czy strona się 'nie zepsuje'...\n:(");
+        Zoom = true;
+
+    }
     var Banner = document.getElementById("Banner");
     var Linki = document.getElementById("LinkiMobile");
     var Content = document.getElementById("Content");
